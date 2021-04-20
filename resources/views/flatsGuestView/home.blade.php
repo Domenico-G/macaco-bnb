dd03001d5bb880a26216a330f5d9b8c9a930591c
Danilo91Colloca <danilo.colloca91@gmail.com>
2021-04-20 20:23:49 +0200

    jumbotronVideo/jumbotronSlide e impostazione card homepage

--- resources/views/flatsGuestView/home.blade.php
+++ resources/views/flatsGuestView/home.blade.php
@@ -7,47 +7,83 @@
         {{-- jumbotron video --}}
         <video playsinline autoplay muted loop>
             <source src="logo-img/jumbotron-video.mp4" type="video/mp4">
+            <source src="logo-img/jumbotron-video.webm" type="video/webm">
+            <source src="logo-img/jumbotron-video.ogg" type="video/ogg">
         </video>
+        {{--
+             Se il browser non dovesse supportare il video compare un carousel
+        --}}
+        <div class="carousel slide bnb-carousel" data-ride="carousel">
+            <div class="carousel-inner bnb-carouselConteiner">
+                <div class="carousel-item active bnb-carousContent">
+                    <img class=" " src="logo-img/villa3.jpeg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class=" " src="logo-img/villa2.jpeg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class=" " src="logo-img/villa4.jpeg" alt="Third slide">
+                </div>
+                <div class="carousel-item">
+                    <img class=" " src="logo-img/villa1.jpeg" alt="Fourth slide">
+                </div>
+            </div>
+        </div>
+
+
+
         {{-- jumbotron text --}}
         <div class="bnb-jumboTextContainer">
             <div class="bnb-textContent">
                 <h1 class="display-4">Macaco B&B</h1>
                 <p class="lead">
-                    “Un viaggio di mille miglia comincia sempre con il primo passo” ...Lao Tzu...
+                    <em>
+                        “Un viaggio di mille miglia comincia sempre con il primo passo”
+                    </em>
+                    <br>
+                    <em style="margin-left: 15px">
+                        - Lao Tzu
+                    </em>
                 </p>
             </div>
 
-            <hr class="bnb-hrStyle">
+            {{-- <hr class="bnb-hrStyle"> --}}
             <p class="lead bnb-buttonContainer">
                 <a class="btn btn-light btn-lg" href="{{route("public.flats.search")}}" role="button">
                     Cerca il tuo Appartamento
                 </a>
             </p>
-
         </div>
-
     </div>
     {{-- flats section --}}
-    <div class="container d-flex flex-wrap bnb-cardContainer">
-
-        @foreach ($flats as $key => $flat)
-            <div class="card col-3 bnb-card">
-                <img class="card-img-top" src="{{$flat->details->image}}" alt="Card image cap">
-                <div class=" bnb-cardBody">
-                    <h5 class="bnb-cardTitle">
-                        {{$flat->details->flat_title}}
-                    </h5>
-                    {{--
-                    <p class="bnb-cardText">
-                        {{$flat->details->description}}
-                    </p>
-                    --}}
-
-                </div>
-                <a href="{{route('public.flats.show', ['flat'=>$flat->id])}}" class="btn btn-primary">
-                    Visualizza Appartamento
-                </a>
+    <div class="bnb-flatsContainer">
+        <div class="container d-flex flex-wrap bnb-cardContainer">
+            <div class="row">
+                @foreach ($flats as $key => $flat)
+                    <div class="col-lg-4 col-md-6 col-xs-12 bnb-card">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class=" card bnb-cardContent">
+                                    <img class="" src="{{$flat->details->image}}" alt="Card image cap">
+                                    <div class=" bnb-cardBody">
+                                        <h5 class="bnb-cardTitle">
+                                            {{$flat->details->flat_title}}
+                                        </h5>
+                                    </div>
+                                    <div class="bnb-buttonContainer">
+                                        <a href="{{route('public.flats.show', ['flat'=>$flat->id])}}" class="btn btn-primary bnb-button">
+                                            Visualizza Appartamento
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
-        @endforeach
+        </div>
     </div>
+
+    {{-- carousel --}}
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,125 +1,123 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Macaco B&B @yield('title')</title>
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+        <title>Macaco B&B @yield('title')</title>
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    {{-- FontAwesome vers. 5.15.1 --}}
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+        {{-- FontAwesome vers. 5.15.1 --}}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light shadow-sm">
-            <div class="container bnb-container">
-                <div class="bnb-logoContainer">
-                    <div class="bnb-brandName">
-                        <span>Macaco B&B</span>
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
+            <nav class="navbar navbar-expand-md navbar-light shadow-sm">
+                <div class="container bnb-container">
+                    <div class="bnb-logoContainer">
+                        <div class="bnb-brandName">
+                            <span>Macaco B&B</span>
+                        </div>
+                        <a class="logo" href="{{ url('/') }}">
+                            <img src="logo-img/macaco-bnb-logo.png" alt="">
+                        </a>
                     </div>
-                    <a class="logo" href="{{ url('/') }}">
-                        <img src="logo-img/macaco-bnb-logo.png" alt="">
 
-                    </a>
-                </div>
+                    <button class="navbar-toggler bnb-buttonStyle" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-                <button class="navbar-toggler bnb-buttonStyle" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item bnb-textContainer">
-                            <a class="nav-link" href="{{ url('/') }}">
-                               Home
-                            </a>
-                        </li>
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item bnb-textContainer">
-                                <a class="nav-link " href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item bnb-textContainer">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                 <!-- Left Side Of Navbar -->
+                            <ul class="navbar-nav mr-auto">
+                                <li class="nav-item bnb-textContainer  bnb-burgerMenu">
+                                    <a class="nav-link" href="{{ url('/') }}">
+                                    Home
                                     </a>
+                                </li>
+                            </ul>
+                            <!-- Right Side Of Navbar -->
+                            <ul class="navbar-nav ml-auto ">
+                                <!-- Authentication Links -->
+                                @guest
+                                    <li class="nav-item bnb-textContainer  bnb-burgerMenu">
+                                        <a class="nav-link " href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    </li>
+                                    @if (Route::has('register'))
+                                        <li class="nav-item bnb-textContainer  bnb-burgerMenu">
+                                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                        </li>
+                                    @endif
+                                @else
+                                    <li class="nav-item dropdown  bnb-burgerMenu">
+                                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                            {{ Auth::user()->name }}
+                                        </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
+                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                            document.getElementById('logout-form').submit();">
+                                                {{ __('Logout') }}
+                                            </a>
 
-                    </ul>
-                </div>
-            </div>
-        </nav>
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                @csrf
+                                            </form>
+                                        </div>
+                                    </li>
+                                @endguest
+                            </ul>
 
-
-        <main class="py-4">
-            @yield("content")
-        </main>
-
-        <footer >
-            <div class="container bnb-container">
-                <div class="col-9 bnb-creditsContainer">
-                    <div class="bnb-creaditsCreators">
-                        Macaco B&B is
-                        created by team 6 with &hearts;
-                        boolean class #25 &copy; ADGGD
                     </div>
                 </div>
-                {{--
-                    sezione contatti social con link esterni alle rispettive pagine
-                --}}
-                <div class="col-3 bnb-rightFoot">
-                    <a href="https://www.facebook.com">
-                        <i class="fab fa-facebook-square"></i>
-                    </a>
-                    <a href="https://www.instagram.com">
-                        <i class="fab fa-instagram"></i>
-                    </a>
-                    <a href="https://www.twitter.com">
-                        <i class="fab fa-twitter-square"></i>
-                    </a>
-                    <a href="https://www.linkedin.com">
-                        <i class="fab fa-linkedin"></i>
-                    </a>
+            </nav>
+
+
+            <main>
+                @yield("content")
+            </main>
+
+            <footer >
+                <div class="container bnb-container">
+                    <div class="col-lg-9 bnb-creditsContainer">
+                        <div class="bnb-creaditsCreators">
+                            Macaco B&B is
+                            created by team 6 with &hearts;
+                            boolean class #25 &copy; ADGGD
+                        </div>
+                    </div>
+                    {{--
+                        sezione contatti social con link esterni alle rispettive pagine
+                    --}}
+                    <div class="col-lg-3 bnb-rightFoot">
+                        <a href="https://www.facebook.com">
+                            <i class="fab fa-facebook-square"></i>
+                        </a>
+                        <a href="https://www.instagram.com">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                        <a href="https://www.twitter.com">
+                            <i class="fab fa-twitter-square"></i>
+                        </a>
+                        <a href="https://www.linkedin.com">
+                            <i class="fab fa-linkedin"></i>
+                        </a>
+                    </div>
                 </div>
-            </div>
-        </footer>
-    </div>
+            </footer>
+        </div>
 </body>
 </html>
